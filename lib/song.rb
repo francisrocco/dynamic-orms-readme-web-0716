74d873d8723393f50422d75f03470e07df2e8cda
Francis <roccofrancis@gmail.com>
2016-08-08 09:16:56 -0400

    Understand Dynamic ORMs

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    column_names = []
+    column_names = [ ]
     table_info.each do |row|
       column_names << row["name"]
     end
